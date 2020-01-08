1f8715c52d4c4cc8f67c084b53ce81c983383dc3
Chris <chris@Chris-E-MacBook-Pro.local>
2020-01-08 14:59:21 +1100

    committ

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+// // adds class of fade to the entire body 
+// document.body.className = 'fade';
+
+// // removes 
+// document.addEventListener("DOMContentLoaded", function(e) {
+//   document.body.className = ''
+// });
+
